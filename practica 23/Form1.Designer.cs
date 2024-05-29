ced7b607d7c794906dd88a1e50507706c8f2c653
admin <admin@USER-PC>
2024-05-29 04:15:43 +0300

    Добавьте файлы проекта.

---
+++ practica 23/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace practica_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.hellodbDataSet = new practica_23.hellodbDataSet();
+            this.hellodbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.naber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.predmet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hellodbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hellodbDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.naber,
+            this.predmet});
+            this.dataGridView1.DataSource = this.hellodbDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(803, 334);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // hellodbDataSet
+            // 
+            this.hellodbDataSet.DataSetName = "hellodbDataSet";
+            this.hellodbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hellodbDataSetBindingSource
+            // 
+            this.hellodbDataSetBindingSource.DataSource = this.hellodbDataSet;
+            this.hellodbDataSetBindingSource.Position = 0;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "name";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.Width = 125;
+            // 
+            // naber
+            // 
+            this.naber.HeaderText = "naber";
+            this.naber.MinimumWidth = 6;
+            this.naber.Name = "naber";
+            this.naber.Width = 125;
+            // 
+            // predmet
+            // 
+            this.predmet.HeaderText = "predmet";
+            this.predmet.MinimumWidth = 6;
+            this.predmet.Name = "predmet";
+            this.predmet.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hellodbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hellodbDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource hellodbDataSetBindingSource;
+        private hellodbDataSet hellodbDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn naber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn predmet;
+    }
+}
+
---
+++ practica 23/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                DataSet MyDataSet = new DataSet();
+                string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
+                //Там где находится файл базы данных
+                string sBank = @"C:\Bases\Animals.mdb";
+                string sSql = "Select * from thisanimals";
+                using (OleDbConnection oleConn = new OleDbConnection(connectionString +
+                sBank))
+                {
+                    try
+                    {
+                        oleConn.Open();
+                        OleDbCommand olecmd = new OleDbCommand(sSql, oleConn);
+                        olecmd.CommandType = CommandType.Text;
+                        OleDbDataAdapter da = new OleDbDataAdapter(olecmd);
+                        da.Fill(MyDataSet);
+                    }
+                    catch (Exception/* ex*/)
+                    {
+                        return;
+                    }
+                }
+                
+            }
+    }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
